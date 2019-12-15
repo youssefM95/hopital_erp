fe525e1e8a692aa14dd83fe74222261ec6bf9192
youssefM95 <youssefmaaloul995@gmail.com>
2019-12-15 19:08:40 +0100

    update module

--- my_addons/hopital/models/medecin.py
+++ my_addons/hopital/models/medecin.py
@@ -12,6 +12,14 @@ class HopitalMedecin(models.Model):
     date_entre = fields.Date('start date')
     prix_heure_operation = fields.Float()
     phone = fields.Char()
-    operation_ids = fields.Many2many(comodel_name='hopital.operationchirurgicale', relation='med_op_rel', column1='f_name',
+    operation_ids = fields.Many2many(comodel_name='hopital.operationchirurgicale', relation='med_op_rel',
+                                     column1='f_name',
                                      column2='o_name')
     specialite_id = fields.Many2one(comodel_name='hopital.specialite')
+    sejours_ids = fields.One2many(comodel_name='hopital.sejourmaladie', inverse_name='medecin_id')
+
+    prix_op = fields.Integer(String='Price Of Operations', compute='prix_operation')
+
+    def prix_operation(self):
+        for operation in self.operation_ids:
+            self.prix_op += operation.nombre_heures * self.prix_heure_operation
--- my_addons/hopital/models/patient.py
+++ my_addons/hopital/models/patient.py
@@ -10,9 +10,17 @@ class HopitalPatient(models.Model):
     identity_card = fields.Char('Identity card')
     address = fields.Text('Address')
     birthday = fields.Date('Birthday')
-    situation_familliale = fields.Selection([('célibataire','célibataire'),('mariée','Mariée'),('divorcé(e)','Divorcé(e)'),('veuf(ve)','Veuf(ve)')])
+    situation_familliale = fields.Selection(
+        [('célibataire', 'célibataire'), ('mariée', 'Mariée'), ('divorcé(e)', 'Divorcé(e)'), ('veuf(ve)', 'Veuf(ve)')])
     email = fields.Char()
     phone = fields.Char()
     operation_ids = fields.Many2many(comodel_name='hopital.operationchirurgicale', relation='pat_op_rel',
                                      column1='f_name',
                                      column2='o_name')
+    sejours_ids = fields.One2many(comodel_name='hopital.sejourmaladie', inverse_name='patient_id')
+    prix_sejour = fields.Integer(String="price of sejour",
+                                 compute='prix_sej')
+
+    def prix_sej(self):
+        for sejour in self.sejours_ids:
+            self.prix_sejour += sejour.nombre_jour * sejour.chambre_id.prix_jour
